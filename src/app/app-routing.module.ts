c82320dcdc4e689f88ddc64fc1e130bf6fbae43b
Marc Grossi <tecnogrossi@gmail.com>
2024-05-29 16:12:35 +0200

    init class 188

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
 
 const routes: Routes = [
-
-
-
-
-
+{
+  path:'auth',
+  loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+},
+{
+  path:'heroes',
+  loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule),
+},
+{
+  path:'404',
+  component: ErrorPageComponent
+},
+{
+  path: '',
+  redirectTo: 'heroes',
+  pathMatch: 'full',
+},
+{
+  path: '**',
+  redirectTo: '404'
+}
 ];
 
 @NgModule({
